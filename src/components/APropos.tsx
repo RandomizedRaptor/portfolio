1ab76dff4dce890fcf17f21822833a69761d1b90
IhabMJR <mouhajerihab7788@gmail.com>
2025-08-12 11:13:07 -0400

    Update APropos.tsx

--- src/components/APropos.tsx
+++ src/components/APropos.tsx
@@ -7,10 +7,7 @@ function aPropos() {
     <div id="aPropos" className="a-propos-container">
         <h1>À propos</h1>
         <p>
-          Étudiant en intégration multimédia, <span className="text-color">je suis passionné par l'interaction entre les divers logiciels de programmation et de design</span>, et j'adore explorer différentes combinaisons d'outils pour créer des expériences uniques et immersives. Mon parcours m'a permis de développer des compétences en programmation tout en explorant les aspects artistiques du design numérique.
-          <br />
-          <br />
-          Actuellement, je suis <span className="text-color">à la recherche d'un stage</span> pour mettre en pratique mes compétences techniques et ma créativité dans des projets <span className="text-color">dans le domaine du web</span> ou dans des projets interactifs, dans le but d'améliorer mes compétences et de progresser dans ma carrière.
+          Ayant étudié en intégration multimédia, <span className="text-color">je suis passionné par l'interaction entre les divers logiciels de programmation et de design</span>, et j'adore explorer différentes combinaisons d'outils pour créer des expériences uniques et immersives. Mon parcours m'a permis de développer des compétences en programmation tout en explorant les aspects artistiques du design numérique.
           <br />
           <br />
           Basé à <span className="text-color">Montréal</span>, Québec.
